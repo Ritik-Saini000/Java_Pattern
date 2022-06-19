1fc1bd7b9ebaa0ce8fe66078c2ed61ff216af846
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-19 18:47:34 +0530

    Add files via upload

---
+++ Pattern12.java
@@ -0,0 +1,19 @@
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char alphabet='A';
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(alphabet);
+			}
+			alphabet++;
+			System.out.println();
+		}
+	}
+
+}
---
+++ Pattern7.java
@@ -0,0 +1,19 @@
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int c=1;
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(c);
+			}
+			c++;
+			System.out.println();
+		}
+	}
+
+}
